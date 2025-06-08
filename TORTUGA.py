ffa67f67641a339889d0d6c9e8b534f3f3827e9f
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-08 15:41:16 -0400

    avance DEL JUEGO TORT

---
+++ TORTUGA.py
@@ -0,0 +1,8 @@
+import turtle
+
+#creacion de las ventanas 
+ventana=turtle.Screen()
+ventana.title('Juego Snake')
+ventana.bgcolor('black')
+ventana.setup(width=600,height=600)
+ventana.mainloop()   
