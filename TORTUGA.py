2eb4e226c75e3f6e40dee834b37eb59e92b43c90
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-09 14:30:47 -0400

    la tortuga

--- TORTUGA.py
+++ TORTUGA.py
@@ -5,4 +5,12 @@ ventana=turtle.Screen()
 ventana.title('Juego Snake')
 ventana.bgcolor('black')
 ventana.setup(width=600,height=600)
+
+##cabeza snake
+cabeza=turtle.Turtle()
+cabeza.speed(0)
+cabeza.penup()
+cabeza.shape('square')
+cabeza.color('blue')
+cabeza.goto(0,0)
 ventana.mainloop()   
