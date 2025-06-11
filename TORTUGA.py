fef59423da119144fa1d94778f8ff5e9d0f9b3df
Nancy Canaviri <nancy.canaviri@sistemas.edu.bo>
2025-06-10 20:59:29 -0400

    funciones de movimiento

--- TORTUGA.py
+++ TORTUGA.py
@@ -1,12 +1,11 @@
 import turtle
 
-#creacion de las ventanas 
+#creacion de la ventana
 ventana=turtle.Screen()
 ventana.title('Juego Snake')
 ventana.bgcolor('black')
 ventana.setup(width=600,height=600)
-
-##cabeza snake
+#funcion creacion de elementos(tortuga)
 def creacion_elemento(forma,color):
     elemento=turtle.Turtle()
     elemento.speed(0)
@@ -15,7 +14,45 @@ def creacion_elemento(forma,color):
     elemento.color(color)
     elemento.goto(0,0)
     return elemento
+#creacion de elementos
 cabeza=creacion_elemento('square','blue')
+cabeza.direccion='quieta'
 comida=creacion_elemento('circle','red')
 comida.goto(0,90)
+
+#funciones
+def arriba():
+    cabeza.direccion='arriba'
+def abajo():
+    cabeza.direccion='abajo'
+def izquierda():
+    cabeza.direccion='izquierda'
+def derecha():
+    cabeza.direccion='derecha'
+
+#teclado
+ventana.listen()
+ventana.onkeypress(arriba,'Up')
+ventana.onkeypress(abajo,'Down')
+ventana.onkeypress(izquierda,'Left')
+ventana.onkeypress(derecha,'Right')
+
+#movimiento
+def movimiento():
+    #movimiento vertical
+    if cabeza.direccion== 'arriba':
+        y=cabeza.ycor()
+        cabeza.sety(y+20)
+    if cabeza.direccion=='abajo':
+        y=cabeza.ycor() 
+        cabeza.sety(y-20)  
+    #movimiento horizontal
+    if cabeza.direccion== 'izquierda':
+        x=cabeza.xcor()
+        cabeza.setx(x-20)
+    if cabeza.direccion=='derecha':
+        x=cabeza.xcor() 
+        cabeza.setx(x+20) 
+
+     
 ventana.mainloop()   
